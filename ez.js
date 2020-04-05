646a00c03b775f96c35af280e7a04ac333652533
ehteshamz <ehtesham.zahoor@nu.edu.pk>
2020-04-05 20:42:49 +0500

    just a basic .js file

---
+++ ez.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class EZ extends Component {
+  render() {
+    return (
+      <div>
+        <h1>hello from ez!</h1>
+        <a href="https://www.shutterstock.com/g/ehtesham/sets/1140147">more...</a>
+      </div>
+    ); }
+}
+
+export default EZ;
